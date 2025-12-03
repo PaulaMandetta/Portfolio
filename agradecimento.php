2b5c4f5b3bc024a55a2bbf464ef86d439a85ffc0
Paula Mandetta Périgo <111874400+PaulaMandetta@users.noreply.github.com>
2025-12-03 13:52:29 -0300

    Add files via upload

--- agradecimento.php
+++ agradecimento.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
     <div class="container text-center mt-5 pt-5 pb-5">
-        <h1>Obrigada por vir até aqui!</h1>
+        <h2 class="display-6 fw-bold mb-0 texto-titulo">Obrigada por vir até aqui!</h2>
         <p><span class="texto-principal">----------------</span></p>
     </div>
 </div>
